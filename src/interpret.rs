dadaa71e46559747a4014570292c0c9d767042ae
Paul Kukowski <kukowskipaul@gmail.com>
2024-10-10 14:35:53 +0200

    Added interpreter, its ugly

--- src/interpret.rs
+++ src/interpret.rs
@@ -2,5 +2,5 @@ use crate::expression::Expression;
 
 pub fn interpret(expr: Box<dyn Expression>) {
     let value = expr.evaluate();
-    eprintln!("{}", value.print_value());
+    println!("{}", value.print_value());
 }
