73534aa25e13b49e6994f409004cb756696f7203
Paul Kukowski <kukowskipaul@gmail.com>
2024-10-10 14:46:46 +0200

    Added a hacky expression parser and interpreter. Coding a dynamically typed language in a type-safe language is a ride

--- src/interpret.rs
+++ src/interpret.rs
@@ -2,7 +2,7 @@ use crate::expression::Expression;
 
 pub fn interpret(expr: Box<dyn Expression>) {
     let value = expr.evaluate();
-    let mut out = value.print_value();
+    let out = value.print_value();
     let out_num = out.parse::<f32>();
     match out_num {
         Ok(f) => {
