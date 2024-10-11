d481b3ea281e3aca1445ee769ab17a1675778acc
Paul Kukowski <kukowskipaul@gmail.com>
2024-10-11 11:53:09 +0200

    Refactored scan.rs to use graphemes because of non-ascii characters

--- src/scan.rs
+++ src/scan.rs
@@ -1,12 +1,14 @@
+use unicode_segmentation::UnicodeSegmentation;
+use regex::Regex;
 use crate::token::{LiteralValue, NumberLiteral, StringLiteral, Token};
 use crate::{report, TokenType, KEYWORDS};
-use std::{fmt, str::FromStr};
+use std::fmt;
 
 type Result<T> = std::result::Result<T, UnexpectedCharacterError>;
 
 #[derive(Debug)]
 enum UnexpectedCharacterError {
-    UnknownCharacter(char),
+    UnknownCharacter(String),
     UnterminatedStringLiteral,
 }
 
@@ -45,6 +47,10 @@ impl Scanner {
     }
 
     pub fn scan_tokens(&mut self) {
+        // let graphemes = self.source.graphemes(true).collect::<Vec<&str>>();
+        // for g in graphemes {
+        //     eprintln!("{g}");
+        // }
         while !self.is_at_end() {
             self.start = self.current;
             match self.scan_token() {
@@ -62,60 +68,61 @@ impl Scanner {
 
     /// Returns true if the current character is the last one in self.source
     fn is_at_end(&self) -> bool {
-        self.current >= self.source.len()
+        let graphemes = self.source.graphemes(true).collect::<Vec<&str>>();
+        self.current >= graphemes.len()
     }
 
     fn scan_token(&mut self) -> Result<()> {
         let c = self.advance().expect("Expected character but found none");
         match c {
             // Single-character tokens
-            '(' => Ok(self.add_token(TokenType::LeftParen)),
-            ')' => Ok(self.add_token(TokenType::RightParen)),
-            '{' => Ok(self.add_token(TokenType::LeftBrace)),
-            '}' => Ok(self.add_token(TokenType::RightBrace)),
-            ',' => Ok(self.add_token(TokenType::Comma)),
-            '.' => Ok(self.add_token(TokenType::Dot)),
-            '-' => Ok(self.add_token(TokenType::Minus)),
-            '+' => Ok(self.add_token(TokenType::Plus)),
-            ';' => Ok(self.add_token(TokenType::Semicolon)),
-            '*' => Ok(self.add_token(TokenType::Star)),
+            "(" => Ok(self.add_token(TokenType::LeftParen)),
+            ")" => Ok(self.add_token(TokenType::RightParen)),
+            "{" => Ok(self.add_token(TokenType::LeftBrace)),
+            "}" => Ok(self.add_token(TokenType::RightBrace)),
+            "," => Ok(self.add_token(TokenType::Comma)),
+            "." => Ok(self.add_token(TokenType::Dot)),
+            "-" => Ok(self.add_token(TokenType::Minus)),
+            "+" => Ok(self.add_token(TokenType::Plus)),
+            ";" => Ok(self.add_token(TokenType::Semicolon)),
+            "*" => Ok(self.add_token(TokenType::Star)),
 
             // Operators can potentially have multiple characters
-            '!' => {
-                let t = if self.match_next('=') {
+            "!" => {
+                let t = if self.match_next("=") {
                     TokenType::BangEqual
                 } else {
                     TokenType::Bang
                 };
                 return Ok(self.add_token(t));
             }
-            '=' => {
-                let t = if self.match_next('=') {
+            "=" => {
+                let t = if self.match_next("=") {
                     TokenType::EqualEqual
                 } else {
                     TokenType::Equal
                 };
                 return Ok(self.add_token(t));
             }
-            '<' => {
-                let t = if self.match_next('=') {
+            "<" => {
+                let t = if self.match_next("=") {
                     TokenType::LessEqual
                 } else {
                     TokenType::Less
                 };
                 return Ok(self.add_token(t));
             }
-            '>' => {
-                let t = if self.match_next('=') {
+            ">" => {
+                let t = if self.match_next("=") {
                     TokenType::GreaterEqual
                 } else {
                     TokenType::Greater
                 };
                 return Ok(self.add_token(t));
             }
-            '/' => {
-                return if self.match_next('/') {
-                    while self.peek() != '\n' && !self.is_at_end() {
+            "/" => {
+                return if self.match_next("/") {
+                    while self.peek() != "\n" && !self.is_at_end() {
                         self.advance();
                     }
                     Ok(())
@@ -125,46 +132,50 @@ impl Scanner {
             }
 
             // '"' begins a string literal
-            '"' => match self.string() {
+            "\"" => match self.string() {
                 Ok(_) => Ok(()),
                 Err(e) => Err(e),
             },
 
             // any digit begins a number literal
-            '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => match self.number() {
+            "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" => match self.number() {
                 Ok(_) => Ok(()),
                 Err(e) => Err(e),
             },
 
             // Newlines
-            '\n' => Ok(self.line += 1),
+            "\n" => Ok(self.line += 1),
 
             // Ignore whitespace
-            ' ' | '\r' | '\t' => Ok(()),
+            " " | "\r" | "\t" => Ok(()),
 
             _ => {
                 // We assume that every alphabetic character starts an identifier
-                if c.is_alphabetic() || c == '_' {
+                if is_alphabetic(c) || c == "_" {
                     return match self.identifier() {
                         Ok(_) => Ok(()),
                         Err(e) => Err(e),
                     };
                 }
                 // Everything else is an unkown character, raise an error
-                Err(UnexpectedCharacterError::UnknownCharacter(c))
+                Err(UnexpectedCharacterError::UnknownCharacter(c.to_owned()))
             }
         }
     }
 
     /// Advances the pointer one position, then
     /// returns the new current character, if there is one
-    fn advance(&mut self) -> Option<char> {
+    fn advance(&mut self) -> Option<&str> {
         self.current += 1;
-        self.source.chars().nth(self.current - 1)
+        let graphemes = self.source.graphemes(true).collect::<Vec<&str>>();
+        if self.current > graphemes.len() {
+            return None;
+        }
+        Some(graphemes[self.current - 1])
     }
 
     /// Returns true if the next character is equal to `expected`
-    fn match_next(&mut self, expected: char) -> bool {
+    fn match_next(&mut self, expected: &str) -> bool {
         if self.is_at_end() {
             return false;
         }
@@ -178,27 +189,24 @@ impl Scanner {
     }
 
     /// Returns the character at the upcoming position, if there is one
-    fn peek(&self) -> char {
+    fn peek(&self) -> &str {
         if self.is_at_end() {
-            return '\0';
+            return "\0";
         }
-        if let Some(c) = self.source.chars().nth(self.current) {
-            return c;
-        } else {
-            return '\0';
-        }
+        let graphemes = self.source.graphemes(true).collect::<Vec<&str>>();
+        graphemes[self.current]
     }
 
     /// Returns the character two positions ahead, if there is one
-    fn peek_next(&self) -> char {
+    fn peek_next(&self) -> &str {
         if self.is_at_end() {
-            return '\0';
+            return "\0";
         }
-        if let Some(c) = self.source.chars().nth(self.current + 1) {
-            return c;
-        } else {
-            return '\0';
+        let graphemes = self.source.graphemes(true).collect::<Vec<&str>>();
+        if self.current < graphemes.len() {
+            return graphemes[self.current + 1];
         }
+        return "\0";
     }
 
     fn add_token(&mut self, token_type: TokenType) {
@@ -207,8 +215,8 @@ impl Scanner {
 
     fn add_literal_token(&mut self, token_type: TokenType, literal: Option<Box<dyn LiteralValue>>) {
         // Parse lexeme from source
-        let text = String::from_str(&self.source[self.start..self.current])
-            .expect("to be able to parse str to String");
+        let graphemes = self.source.graphemes(true).collect::<Vec<&str>>();
+        let text = graphemes[self.start..self.current].concat();
         self.tokens
             .push(Token::new(token_type, text, literal, self.line));
     }
@@ -217,8 +225,8 @@ impl Scanner {
         let mut lines: usize = 0;
 
         // While we haven't reached the closing " or the end of the line, advance
-        while self.peek() != '"' && !self.is_at_end() {
-            if self.peek() == '\n' {
+        while self.peek() != "\"" && !self.is_at_end() {
+            if self.peek() == "\n" {
                 lines += 1;
                 self.line += lines;
             }
@@ -236,9 +244,9 @@ impl Scanner {
         self.advance();
 
         // Parse the string literals value from source
+        let graphemes = self.source.graphemes(true).collect::<Vec<&str>>();
         let literal = StringLiteral {
-            value: String::from_str(&self.source[self.start + 1..self.current - 1])
-                .expect("to be able to parse str to String"),
+            value: graphemes[self.start + 1..self.current - 1].concat()
         };
 
         self.add_literal_token(TokenType::String, Some(Box::new(literal)));
@@ -247,21 +255,21 @@ impl Scanner {
 
     fn number(&mut self) -> Result<()> {
         // Keep parsing while the next character is numeric
-        while self.peek().is_digit(10) {
+        while is_digit(self.peek()) {
             self.advance();
         }
 
         // If the next character is a decimal point AND the character after that is numeric,
         // keep parsing
-        if self.peek() == '.' && self.peek_next().is_digit(10) {
+        if self.peek() == "." && is_digit(self.peek_next()) {
             self.advance();
-            while self.peek().is_numeric() {
+            while is_digit(self.peek()) {
                 self.advance();
             }
         }
 
-        // TODO: this feels dirty and I don't like it
-        let value_str = &self.source[self.start..self.current];
+        let graphemes = self.source.graphemes(true).collect::<Vec<&str>>();
+        let value_str = graphemes[self.start..self.current].concat();
         let literal = NumberLiteral {
             value: value_str
                 .parse()
@@ -272,14 +280,14 @@ impl Scanner {
         Ok(())
     }
 
-    // TODO: does this need a result? When would this error?
     fn identifier(&mut self) -> Result<()> {
         // Keep parsing while the next character is alphanumeric or an underscore _
-        while self.peek().is_alphanumeric() || self.peek() == '_' {
+        while is_alphabetic(self.peek()) || is_digit(self.peek()) || self.peek() == "_" {
             self.advance();
         }
-        let value_str = &self.source[self.start..self.current];
-        if let Some(identifier_type) = KEYWORDS.lock().unwrap().get(value_str) {
+        let graphemes = self.source.graphemes(true).collect::<Vec<&str>>();
+        let value_str = graphemes[self.start..self.current].concat();
+        if let Some(identifier_type) = KEYWORDS.lock().unwrap().get(value_str.as_str()) {
             self.add_token(identifier_type.clone());
             return Ok(());
         } else {
@@ -296,4 +304,16 @@ impl fmt::Display for Scanner {
         }
         Ok(())
     }
+}
+
+fn is_digit(grapheme: &str) -> bool {
+    match grapheme {
+        "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" => return true,
+        _ => return false
+    }
+}
+
+fn is_alphabetic(grapheme: &str) -> bool {
+    let re = Regex::new(r"[a-zA-Z]").unwrap();
+    re.is_match(grapheme)
 }
